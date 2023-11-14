8e716dd7e70fb0d29f17092980dba907cd1675b8
joshua <ngmerteyjoshua360@gmail.com>
2023-11-14 12:10:11 -0800

    a function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+/* #define BUFF_SIZE 1024 */
+
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
@@ -26,7 +28,6 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -38,9 +39,12 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+					flags, width, precision, size);
 			if (printed == -1)
+			{
+				va_end(list);
 				return (-1);
+			}
 			printed_chars += printed;
 		}
 	}
@@ -55,12 +59,14 @@ int _printf(const char *format, ...)
 /**
  * print_buffer - main func prints the contents of the buffer
  * @buffer: Array of characters
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buff_ind: Index at which to add the next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
+	{
 		write(1, &buffer[0], *buff_ind);
+	}
 
 	*buff_ind = 0;
 }
